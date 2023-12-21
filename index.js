9c09acb72bdceb23ba3f38c1df724cc417218b52
Filip <filipkowalewski@mailfence.com>
2023-12-21 17:01:23 +0100

    big siur

--- index.js
+++ index.js
@@ -1,53 +1,137 @@
-const cors = require('cors')
-const express = require('express')
-const app = express()
-const port = 3000
+const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+const express = require('express');
+const jwt = require('jsonwebtoken');
+const path = require('path');
+const { Pool } = require('pg');
 
-app.use(cors())
+const misc = require('./modules/misc');
 
-const LinkManager = require("./link_manager")
+const jwtSecretKey = 'jwtSecretKey';
+const port = 3000;
 
-const link_manager = new LinkManager()
+const pool = new Pool({
+    user: 'siuras',
+    host: 'localhost',
+    database: 'siuras',
+    password: 'siuras',
+    port: 5432,
+});
 
-app.get('/', (req, res) => {
-    res.send('Patryk2703')
-})
+const app = express();
 
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+app.set('views', path.join(__dirname, 'views'));
 
-app.get('/add', (req, res) => {
-    let url = req.query.url;
-    let short_name = req.query.short_name;
-    if (!short_name) {
-        // RANDOM NAME
-        short_name = (Math.random() + 1).toString(36).substring(7);
-        console.log("RANDOMMMMM")
-        console.log(short_name)
-        console.log(':<')
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.static('public'));
+
+const getTokenData = (req, res, next) => {
+    if (!req.cookies.token) {
+        next();
+        return
     }
 
-    link_manager.addShortcutFromUrl(
-        short_name,
-        url
-    )
-    res.status(201)
-    res.send({ short_name })
+    jwt.verify(req.cookies.token, jwtSecretKey, (err, decoded) => {
+        if (!err) req.user = decoded;
+        next();
+    });
+};
+
+app.get('/', getTokenData, (req, res) => {
+    res.render('index', { user: req.user });
 })
 
-app.get('/get/:short_name/', (req, res) => {
-    let url = link_manager.getUrlFromShortcut(
-        req.params.short_name
-    )
-    if (url) {
-        // res.status(200)
-        // res.send(url)
-        res.status(301)
-        res.redirect(url)
-    } else {
-        res.status(404)
-        res.send(`Not found: ${req.params.short_name}`)
+app.get('/login', (req, res) => {
+    if (req.cookies.token) return res.redirect('/')
+    res.sendFile(path.join(__dirname, 'views', 'login.html'));
+})
+
+app.post('/login', async (req, res) => {
+    const { username, password } = req.body;
+    const result = await pool.query('SELECT id, username, password FROM users WHERE username = $1', [username])
+    if (result.rows.length === 0) {
+        return res.status(401).send(`Niepoprawna nazwa użytkownika lub hasło.`);
     }
+    const user = result.rows[0]
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (isPasswordValid !== true) {
+        return res.status(401).send(`Niepoprawna nazwa użytkownika lub hasło.`);
+    }
+    const token = jwt.sign({ id: user.id, username: user.username }, jwtSecretKey)
+    res.cookie('token', token, { httpOnly: true })
+    res.redirect('/')
 })
 
-app.listen(port, () => {
+app.get('/logout', (req, res) => {
+    if (req.cookies.token) res.clearCookie('token')
+    res.redirect('/')
+})
+
+app.get('/register', (req, res) => {
+    if (req.cookies.token) return res.redirect('/')
+    res.sendFile(path.join(__dirname, 'views', 'register.html'));
+})
+
+app.post('/register', async (req, res) => {
+    const { username, password0, password1 } = req.body;
+    if (password0 !== password1) {
+        return res.status(401).json('Hasła nie są takie same.')
+    }
+    const usernameExists = await pool.query('SELECT 1 FROM users WHERE username = $1', [username])
+    if (usernameExists.rows.length !== 0) {
+        return res.status(409).send(`Użytkownik o nazwie ${username} już istnieje.`);
+    }
+    const hashedPassword = await bcrypt.hash(password0, 10);
+    await pool.query('INSERT INTO users (username, password) VALUES ($1, $2)', [username, hashedPassword])
+    res.redirect(`/login?registrationSuccess=true&newUserName=${username}`)
+})
+
+app.post('/', getTokenData, async (req, res) => {
+    let { url, name } = req.body;
+    name = name || misc.getRandomName()
+    if (['', 'login', 'logout', 'register', 'your-urls'].includes(name)) {
+        return res.status(403).json({ message: 'Niepoprawna nazwa linku, wybierz inną nazwę.' })
+    }
+
+    const result = await pool.query('SELECT 1 FROM urls WHERE name = $1', [name])
+    if (result.rows.length !== 0) {
+        return res.status(403).json({ message: 'Nazwa linku już zajęta, wybierz inną nazwę.' })
+    }
+
+    if (req.user && req.user.id)
+        await pool.query('INSERT INTO urls (url, name, user_id) VALUES ($1, $2, $3)', [url, name, req.user.id])
+    else
+        await pool.query('INSERT INTO urls (url, name) VALUES ($1, $2)', [url, name])
+
+    const niceUrl = `${req.protocol}://${req.headers.host}/${name}`
+    res.status(201).json({ url, niceUrl })
+})
+
+app.get('/your-urls', getTokenData, async (req, res) => {
+    const user = req.user;
+    if (!user) return res.redirect('/')
+    const urlBeforeName = `${req.protocol}://${req.headers.host}/`
+    const result = await pool.query('SELECT name, url FROM urls where user_id = $1', [user.id])
+    const urls = result.rows.map((row) => { return { ...row, niceUrl: `${urlBeforeName}${row.name}` } });
+    res.render('urls', { username: user.username, urls });
+})
+
+app.get('/:name', async (req, res) => {
+    const name = req.params.name
+    const result = await pool.query('SELECT url FROM urls WHERE name = $1', [name])
+    if (result.rowCount === 0) {
+        return res.sendFile(path.join(__dirname, 'views', '404.html'));
+    }
+    res.redirect(result.rows[0].url)
+})
+
+app.listen(port, async () => {
     console.log(`UFO ${port}`)
 })
--- public/js/index.js
+++ public/js/index.js
@@ -1,8 +1,6 @@
-const apiUrl = 'http://127.0.0.1:3000'
-
 const urlInput = document.getElementById('url-input')
 const saveBtn = document.getElementById('save-btn')
-const message = document.getElementById('message')
+const serverResult = document.getElementById('server-result')
 
 const checkbox = document.getElementById('user-url-name-checkbox')
 const urlNameInput = document.getElementById('user-url-name-input')
@@ -15,35 +13,34 @@ checkbox.addEventListener('change', () => {
 })
 
 saveBtn.addEventListener('click', async () => {
-    console.log('saving')
     let tmpErrorMessage = null
-    const queryParams = {url: urlInput.value}
+    const requestBody = { url: urlInput.value }
     if (urlInput.value === '') {
         tmpErrorMessage = 'Nie podałeś linku gościu.'
     } else if (checkbox.checked === true && urlNameInput.value === '') {
         tmpErrorMessage = 'Nie podałeś nazwy do skróconego linku.'
     } else if (checkbox.checked === true && urlNameInput.value !== '') {
-        queryParams['short_name'] = urlNameInput.value
+        requestBody['name'] = urlNameInput.value
     }
 
     if (tmpErrorMessage !== null) {
-        message.textContent = tmpErrorMessage
-        if (message.classList.contains('hidden')) message.classList.remove('hidden')
+        serverResult.innerHTML = ``
         return
     }
 
-    const url = new URL(`${apiUrl}/add`)
-    Object.keys(queryParams).forEach(key => url.searchParams.append(key, queryParams[key]))
-    const res = await fetch(url)
+    const res = await fetch('/', { method: 'POST', headers: { 'Content-Type': 'application/json' }, body: JSON.stringify(requestBody) })
+    if (res.status >= 400) {
+        serverResult.innerHTML = `<p class="error">Coś nie tak.</p>`
+        return
+    }
     const data = await res.json()
-    console.log(data)
-    message.textContent = `${apiUrl}/get/${data.short_name}`
-    message.classList.add('success')
-    message.classList.remove('hidden')
+
+    if (res.status !== 201) {
+        serverResult.innerHTML = `<p class="error">${data.message}</p>`
+        return
+    }
+    serverResult.innerHTML = `
+        <p>Proszę, to twój skrucony link:</p>
+        <input type="text" class="input-0" value="${data.niceUrl}" readonly/>
+    `
 })
-
-const redirectoToPage = () => {
-    console.log('redirecting...')
-}
-
-window.onload = redirectoToPage
