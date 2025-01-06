af45011995e6e047fd829c0217c082a8ef3ddd8b
FilipK0walewski <filipkowalewski00@gmail.com>
2025-01-06 12:19:07 +0100

    password check

--- public/js/register.js
+++ public/js/register.js
@@ -2,19 +2,29 @@ const registerForm = document.getElementById('register-form')
 const message = document.getElementById('message')
 
 const displayMessage = (text) => {
-    message.textContent = text
+    let messageText = text.toUpperCase()
+    if (messageText.charAt(messageText.length - 1) !== '!') {
+        messageText += '!'
+    }
+    message.textContent = messageText 
     if (message.classList.contains('hidden')) message.classList.remove('hidden')
 }
 
 registerForm.addEventListener('submit', async (event) => {
     event.preventDefault()
     const formData = new FormData(registerForm);
+
+    const username = formData.get('username').trim()
+    const password0 = formData.get('password0').trim()
+    const password1 = formData.get('password1').trim()
+
+    if (password0 !== password1) {
+        displayMessage('Hasła nie są takie same')
+        return
+    }
+
     try {
-        const body = JSON.stringify({
-            username: formData.get('username'),
-            password0: formData.get('password0'),
-            password1: formData.get('password1')
-        })
+        const body = JSON.stringify({username, password0, password1})
         const response = await fetch('/register', { method: 'POST', headers: { 'Content-Type': 'application/json' }, body })
         const data = await response.json()
         if (response.status === 201) {
